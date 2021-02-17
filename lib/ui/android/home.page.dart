1301564a02c1f3e81637a2862de1d0f8c9bd30ca
Thiago Nogueira <thiagotn@gmail.com>
2021-02-17 09:37:20 -0300

    adicionando listagem de categorias na home

--- lib/ui/android/home.page.dart
+++ lib/ui/android/home.page.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shoppingfull/blocs/home.bloc.dart';
+import 'package:shoppingfull/shared/widgets/category/category-list.widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final HomeBloc bloc = Provider.of<HomeBloc>(context);
+
     return Scaffold(
-      body: Container(
-        color: Colors.red,
-        child: Center(
-          child: Text("Shopping Cart"),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              "Categorias",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CategoryList(
+              categories: bloc.categories,
+            ),
+          ],
         ),
       ),
     );
